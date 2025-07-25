c4daeeebdfbf77a86179153e484ec211148fbfcc
Misbaul-hasan-jisan <jisanaa22@gmail.com>
2025-07-25 23:54:26 +0600

    frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     Sitemap({
       hostname: 'https://rs-engineering.pages.dev',
       routes: [
-        '/', // Home
+        '/',
         '/computer',
         '/electronics',
         '/fashion',
