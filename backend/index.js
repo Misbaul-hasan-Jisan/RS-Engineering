48e9d6572254ac7bbc55acdf08b9b5a7cf99fc2f
Misbaul-hasan-jisan <jisanaa22@gmail.com>
2025-08-04 17:53:55 +0600

    backend

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, auth-token');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Max-Age', '86400');
